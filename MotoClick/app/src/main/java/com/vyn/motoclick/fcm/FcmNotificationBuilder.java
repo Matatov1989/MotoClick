675ea842331e3debb48eeffbd25d7d1a7d351699
Matatov1989 <Matatov1989@gmail.com>
2017-06-25 17:34:59 +0300

    Fixed bug

--- MotoClick/app/src/main/java/com/vyn/motoclick/fcm/FcmNotificationBuilder.java
+++ MotoClick/app/src/main/java/com/vyn/motoclick/fcm/FcmNotificationBuilder.java
@@ -18,7 +18,7 @@ import okhttp3.Response;
 
 public class FcmNotificationBuilder {
     public static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
-    private static final String SERVER_API_KEY = "AAAAh521a2c:APA91bEn7PQclOC_hEVfaSNTJSvvpC5v6kOrQDxEurgPsJj6T0zrrAA35e4Nj4I5vlEsO43fvUSiUV8DRYKhksU-7ciLuDSgzM5lUVFXDZ8Zu31tur-XIocJpj6ZEOI4hjPkn1vZV7pR";
+    private static final String SERVER_API_KEY = "";
     private static final String CONTENT_TYPE = "Content-Type";
     private static final String APPLICATION_JSON = "application/json";
     private static final String AUTHORIZATION = "Authorization";
